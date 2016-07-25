33191bfbd899d36556188c096c286c72a700ea5f
Mike Jolley <mike.jolley@me.com>
2016-07-25 11:51:30 +0100

    Support ajax cart

--- assets/js/wc-quantity-increment.js
+++ assets/js/wc-quantity-increment.js
@@ -1,57 +1,56 @@
-if (!String.prototype.getDecimals) {
-	String.prototype.getDecimals = function() {
-		var num = this,
-			match = ('' + num).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
-		if (!match)
-			return 0;
-		return Math.max(0, (match[1] ? match[1].length : 0) - (match[2] ? +match[2] : 0));
+jQuery( function( $ ) {
+
+	if ( ! String.prototype.getDecimals ) {
+		String.prototype.getDecimals = function() {
+			var num = this,
+				match = ('' + num).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+			if ( ! match ) {
+				return 0;
+			}
+			return Math.max( 0, ( match[1] ? match[1].length : 0 ) - ( match[2] ? +match[2] : 0 ) );
+		}
 	}
-}
 
-function wcqi_refresh_quantity_increments(){
-	// Quantity buttons
-	jQuery( 'div.quantity:not(.buttons_added), td.quantity:not(.buttons_added)' ).addClass( 'buttons_added' ).append( '<input type="button" value="+" class="plus" />' ).prepend( '<input type="button" value="-" class="minus" />' );
-}
+	function wcqi_refresh_quantity_increments(){
+		$( 'div.quantity:not(.buttons_added), td.quantity:not(.buttons_added)' ).addClass( 'buttons_added' ).append( '<input type="button" value="+" class="plus" />' ).prepend( '<input type="button" value="-" class="minus" />' );
+	}
 
-jQuery(document).ready(function(){
-	wcqi_refresh_quantity_increments();
-});
+	$( document ).on( 'updated_wc_div', function() {
+		wcqi_refresh_quantity_increments();
+	} );
 
-jQuery( document ).on( 'click', '.plus, .minus', function() {
+	$( document ).on( 'click', '.plus, .minus', function() {
+		// Get values
+		var $qty		= $( this ).closest( '.quantity' ).find( '.qty'),
+			currentVal	= parseFloat( $qty.val() ),
+			max			= parseFloat( $qty.attr( 'max' ) ),
+			min			= parseFloat( $qty.attr( 'min' ) ),
+			step		= $qty.attr( 'step' );
 
-	// Get values
-	var $qty		= jQuery( this ).closest( '.quantity' ).find( '.qty'),
-		currentVal	= parseFloat( $qty.val() ),
-		max			= parseFloat( $qty.attr( 'max' ) ),
-		min			= parseFloat( $qty.attr( 'min' ) ),
-		step		= $qty.attr( 'step' );
-	
-	// Format values
-	if ( ! currentVal || currentVal === '' || currentVal === 'NaN' ) currentVal = 0;
-	if ( max === '' || max === 'NaN' ) max = '';
-	if ( min === '' || min === 'NaN' ) min = 0;
-	if ( step === 'any' || step === '' || step === undefined || parseFloat( step ) === 'NaN' ) step = 1;
+		// Format values
+		if ( ! currentVal || currentVal === '' || currentVal === 'NaN' ) currentVal = 0;
+		if ( max === '' || max === 'NaN' ) max = '';
+		if ( min === '' || min === 'NaN' ) min = 0;
+		if ( step === 'any' || step === '' || step === undefined || parseFloat( step ) === 'NaN' ) step = 1;
 
-	// Change the value
-	if ( jQuery( this ).is( '.plus' ) ) {
-
-		if ( max && ( currentVal >= max ) ) {
-			$qty.val( max );
+		// Change the value
+		if ( $( this ).is( '.plus' ) ) {
+			if ( max && ( currentVal >= max ) ) {
+				$qty.val( max );
+			} else {
+				$qty.val( ( currentVal + parseFloat( step )).toFixed( step.getDecimals() ) );
+			}
 		} else {
-			$qty.val( (currentVal + parseFloat( step )).toFixed(step.getDecimals()) );
+			if ( min && ( currentVal <= min ) ) {
+				$qty.val( min );
+			} else if ( currentVal > 0 ) {
+				$qty.val( ( currentVal - parseFloat( step )).toFixed( step.getDecimals() ) );
+			}
 		}
 
-	} else {
+		// Trigger change event
+		$qty.trigger( 'change' );
+	});
 
-		if ( min && ( currentVal <= min ) ) {
-			$qty.val( min );
-		} else if ( currentVal > 0 ) {
-			$qty.val( (currentVal - parseFloat( step )).toFixed(step.getDecimals()) );
-		}
-
-	}
-
-	// Trigger change event
-	$qty.trigger( 'change' );
-
+	wcqi_refresh_quantity_increments();
 });
